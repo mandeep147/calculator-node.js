d978fd794638f52fb5f912431db5b6a50e7eebbe
mandeep147 <mndpkaur14@gmail.com>
2016-06-29 23:45:52 +0530

    Calculator operation with GET REquest

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http'),
     url = require('url'),
+    bodyParser = require('body-parser');
     express = require('express');
 
 var app = express();
@@ -10,27 +11,51 @@ app.engine('.html', require('ejs').renderFile);
 app.set('port', process.env.PORT||3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(bodyParser.json());   
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',function (req, res) {
+
 	res.render('main.html');
 });
 
-app.get('/add', function(req, res){
-	res.render('add.html');
-})
+app.get('/process', function(req, res){
+/*response = {
+       a : req.query.a,
+       b : req.query.b
+   };
+  */
+   var a = parseInt(req.query.a);
+   var b = parseInt(req.query.b);
 
-app.get('/div', function(req, res){
-	res.render('div.html');
-})
+//   var c = parseInt(a) + parseInt(b);
+  var c;
+	//console.log(req);
 
-app.get('/mult', function(req, res){
-	res.render('mult.html');
-})
+	if(req.query.hasOwnProperty('Add')){
+		c = a + b;
+	}
+	else if (req.query.hasOwnProperty('Subtract')) {
+		c = a - b;
+	}
+	else if (req.query.hasOwnProperty('Multiply')) {
+		c = a * b;
+	}
+	else if (req.query.hasOwnProperty('Divide')) {
+		if(b != 0){
+			c = a / b;
+		}
+		else 
+			c = 'b should not be 0';
+	}
+  // console.log(response);
+   //res.end(JSON.stringify(response));
+	//res.render('add.html');
+	res.send(c.toString());
 
-app.get('/sub', function(req, res){
-	res.render('sub.html');
-})
+});
 
 server.listen(app.get('port'),function () {
 	console.log(' server is listening on port ' + app.get('port'));
+ 	
 });
